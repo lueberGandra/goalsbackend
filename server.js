9c5f597f606a277fd2665bdf6ab8da198546d142
lueberGandra <lueber_emp@hotmail.com>
2022-08-15 16:13:42 -0300

    REST API for goals

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT|| 5000;
+const { errorHandler } = require("./src/middlewares/errorMiddleware");
+const connectDB = require("./src/config/db")
+const port = process.env.PORT || 5000;
+
+connectDB()
 
 const app = express();
 
-app.use('/api/goals',require('./src/routes/goalRoutes'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./src/routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/controllers/goalControllers.js
+++ src/controllers/goalControllers.js
@@ -1,30 +1,69 @@
+const asyncHandler = require("express-async-handler");
+
+const Goal = require("../models/goalModel");
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
-  res.status(200).json({ msg: "Get goals" });
-};
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
+});
 
 // @desc Create Goals
 // @route POST /api/goals
 // @access Private
-const createGoal = (req, res) => {
-  res.status(201).json({ msg: "Set goal" });
-};
+const createGoal = asyncHandler(async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400);
+    throw new Error(`'name' field is required!`);
+  }
+  const goal = await Goal.create({ name });
+  res.status(201).json(goal);
+});
 
 // @desc Update Goals
 // @route PUT /api/goals
 // @access Private
-const updateGoal = (req, res) => {
-  res.status(200).json({ msg: `Update goal ${req.params.id}` });
-};
+const updateGoal = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    res.status(400);
+    throw new Error(`'name' field is required!`);
+  }
+
+  const goal = await Goal.findById(id);
+  if (!goal) {
+    res.status(400);
+    throw new Error(`Goal not found`);
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(id, { name }, { new: true });
+
+  res.status(200).json(updatedGoal);
+});
 
 // @desc Delete Goals
 // @route Delete /api/goals
 // @access Private
-const deleteGoal = (req, res) => {
-  res.status(200).json({ msg: `Delete goal ${req.params.id}` });
-};
+const deleteGoal = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const goal = await Goal.findById(id);
+  if (!goal) {
+    res.status(400);
+    throw new Error(`Goal not found`);
+  }
+  await goal.remove();
+
+  res
+    .status(200)
+    .json({ message: `'${goal.name}' has been deleted successfully` });
+});
 
 module.exports = {
   getGoals,
